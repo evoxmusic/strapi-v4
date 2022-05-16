dc8dfe7b3addcd7d1eff400c3deee0fc8de582de
Romaric Philogène <rphilogene@qovery.com>
2022-05-16 11:51:33 +0200

    change config database to use Postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,30 @@
 const path = require('path');
 
+//module.exports = ({ env }) => ({
+//  connection: {
+//    client: 'postgres',
+//    connection: {
+//      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//    },
+//    useNullAsDefault: true,
+//  },
+//});
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "bank"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "0000"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
