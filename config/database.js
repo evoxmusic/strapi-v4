c44a4ac1abbc930ec33170210f4db93b1060f3a5
Romaric Philogène <rphilogene@qovery.com>
2022-05-16 22:49:41 +0200

    turn off ssl check

--- config/database.js
+++ config/database.js
@@ -20,9 +20,10 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      //ssl: {
+      //  rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      //},
+      ssl: false,
     },
     debug: false,
   },
